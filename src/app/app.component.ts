21110811a46b37551dab950d992420d481015537
paultanbunan <paul.tanbunan@gmail.com>
2021-02-23 20:53:19 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,54 +16,20 @@ export class AppComponent {
   ];
   x;
   y;
-  player1 = 0;
-  player2 = 0;
+  a = "player 1 menang!";
+  b = "player 2 menang!";
   click() {
     if (this.turn == 1) {
       if (this.board[this.x - 1][this.y - 1] == 0) {
         this.board[this.x - 1][this.y - 1] = 1;
-        for (var i = 0; i < 5; i++) {
-          for (var j = 0; j < 4; j++) {
-            if (this.board[j][i] == 1 && this.board[j + 1][i] == 1) {
-              this.player1++;
-            }
-            if (this.board[j][i] == 2 && this.board[j + 1][i] == 2) {
-              this.player2++;
-            }
-          }
-        }
         this.turn = 2;
-        console.log(this.player1);
-        if (this.player1 == 5) {
-          alert("Player 1 Menang");
-        }
-        if (this.player2 == 5) {
-          alert("Player 2 Menang");
-        }
       } else {
         alert("Sudah Diinput");
       }
     } else {
       if (this.board[this.x - 1][this.y - 1] == 0) {
         this.board[this.x - 1][this.y - 1] = 2;
-        for (var i = 0; i < 5; i++) {
-          for (var j = 0; j < 4; j++) {
-            if (this.board[j][i] == 1 && this.board[j + 1][i] == 1) {
-              this.player1++;
-            }
-            if (this.board[j][i] == 2 && this.board[j + 1][i] == 2) {
-              this.player2++;
-            }
-          }
-        }
         this.turn = 1;
-        console.log(this.player1);
-        if (this.player1 == 5) {
-          alert("Player 1 Menang");
-        }
-        if (this.player2 == 5) {
-          alert("Player 2 Menang");
-        }
       } else {
         alert("Sudah Diinput");
       }
