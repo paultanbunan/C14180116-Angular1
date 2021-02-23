809cde377220af26a9b808a9aef2ddf14ab2465b
paultanbunan <paul.tanbunan@gmail.com>
2021-02-23 20:38:50 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component, VERSION } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  hasil;
   turn = 1;
   board: number[][] = [
     [0, 0, 0, 0, 0],
@@ -17,24 +16,56 @@ export class AppComponent {
   ];
   x;
   y;
+  player1 = 0;
+  player2 = 0;
   click() {
     if (this.turn == 1) {
       if (this.board[this.x - 1][this.y - 1] == 0) {
         this.board[this.x - 1][this.y - 1] = 1;
+        for (var i = 0; i < 5; i++) {
+          for (var j = 0; j < 4; j++) {
+            if (this.board[j][i] == 1 && this.board[j + 1][i] == 1) {
+              this.player1++;
+            }
+            if (this.board[j][i] == 2 && this.board[j + 1][i] == 2) {
+              this.player2++;
+            }
+          }
+        }
         this.turn = 2;
-        this.hasil = this.board;
-        console.log(this.hasil);
+        console.log(this.player1);
+        if (this.player1 == 5) {
+          alert("Player 1 Menang");
+        }
+        if (this.player2 == 5) {
+          alert("Player 2 Menang");
+        }
       } else {
-        alert("Sudah Diinput Player 2");
+        alert("Sudah Diinput");
       }
     } else {
       if (this.board[this.x - 1][this.y - 1] == 0) {
         this.board[this.x - 1][this.y - 1] = 2;
+        for (var i = 0; i < 5; i++) {
+          for (var j = 0; j < 4; j++) {
+            if (this.board[j][i] == 1 && this.board[j + 1][i] == 1) {
+              this.player1++;
+            }
+            if (this.board[j][i] == 2 && this.board[j + 1][i] == 2) {
+              this.player2++;
+            }
+          }
+        }
         this.turn = 1;
-        this.hasil = this.board;
-        console.log(this.hasil);
+        console.log(this.player1);
+        if (this.player1 == 5) {
+          alert("Player 1 Menang");
+        }
+        if (this.player2 == 5) {
+          alert("Player 2 Menang");
+        }
       } else {
-        alert("Sudah Diinput Player 1");
+        alert("Sudah Diinput");
       }
     }
   }
