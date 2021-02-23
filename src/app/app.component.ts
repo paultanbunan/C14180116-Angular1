e806ba61d9c0fec9d7120ad0dba21259c8b1c8ec
paultanbunan <paul.tanbunan@gmail.com>
2021-02-23 19:57:01 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  hasil;
+  giliran = true;
+  board: number[][] = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+  ];
+  x;
+  y;
+  click() {
+    if (this.giliran) {
+      if (this.board[this.x - 1][this.y - 1] == 0) {
+        this.board[this.x - 1][this.y - 1] = 1;
+        this.giliran = false;
+        this.hasil = this.board;
+        console.log(this.hasil);
+      } else {
+        alert(this.hasil);
+      }
+    } else {
+      if (this.board[this.x - 1][this.y - 1] == 0) {
+        this.board[this.x - 1][this.y - 1] = 2;
+        this.giliran = true;
+        this.hasil = this.board;
+        console.log(this.hasil);
+      } else {
+        alert(this.hasil);
+      }
+    }
+  }
 }
